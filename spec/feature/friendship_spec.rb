03c62312504b9518a0846108b5e5e368ae5ba2f4
JasemDunan <jasemvalencia@gmail.com>
2020-11-12 16:34:06 -0500

    Create test ecenario for friend reject and accept

--- spec/feature/friendship_spec.rb
+++ spec/feature/friendship_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe 'Testing Friendship features', type: :feature, feature: true do
             fill_in 'Email', with: 'hans@gmail.com'
             fill_in 'Password', with: '123456'
             click_on 'Log in'
+            # puts @user.id
+            # puts @user2.id
+            # puts 'before'
         end
 
         scenario 'log_in valid' do
@@ -15,7 +18,7 @@ RSpec.describe 'Testing Friendship features', type: :feature, feature: true do
         end
 
         scenario 'friend request' do
-            visit 'http://localhost:3000/users'
+            # visit 'http://localhost:3000/users'
             url = 'http://localhost:3000/users/'
             url.concat(@user2.id.to_s,'/friendships')
             page.driver.submit :post, url, {}            
@@ -24,11 +27,13 @@ RSpec.describe 'Testing Friendship features', type: :feature, feature: true do
         end
 
         scenario 'friend accept' do
-            visit 'http://localhost:3000/users'
-            url = 'http://localhost:3000/friendships/'
-            url.concat(@user2.id.to_s)
-            visit url
-            click_on 'Sign out'
+            # request  
+            url = 'http://localhost:3000/users/'
+            url.concat(@user2.id.to_s,'/friendships')
+            page.driver.submit :post, url, {}            
+            expect(@user.pending_friends).to include (@user2)
+            click_on 'Sign out'    
+            # Accept           
             visit 'http://localhost:3000/users/sign_in'
             fill_in 'Email', with: 'yaser@gmail.com'
             fill_in 'Password', with: '123456'
@@ -37,14 +42,17 @@ RSpec.describe 'Testing Friendship features', type: :feature, feature: true do
             url = 'http://localhost:3000/friendships/'
             url.concat(@user.id.to_s)
             page.driver.submit :patch, url, {}
-            expect(page).to have_content('Accepted Friendship')
+            expect(page).to have_content('You accept an Invitation')
         end
 
         scenario 'friend reject' do 
-            visit 'http://localhost:3000/users'
-            url = 'http://localhost:3000/friendships/'
-            url.concat(@user2.id.to_s)
-            visit url
+            # visit 'http://localhost:3000/users'
+            # request
+            url = 'http://localhost:3000/users/'
+            url.concat(@user2.id.to_s,'/friendships')
+            page.driver.submit :post, url, {}            
+            expect(@user.pending_friends).to include (@user2) 
+            # Reject
             click_on 'Sign out'
             visit 'http://localhost:3000/users/sign_in'
             fill_in 'Email', with: 'yaser@gmail.com'
@@ -53,19 +61,10 @@ RSpec.describe 'Testing Friendship features', type: :feature, feature: true do
             visit 'http://localhost:3000/users'
             url = 'http://localhost:3000/friendships/'
             url.concat(@user.id.to_s)
+            # puts url
             page.driver.submit :delete, url, {}
-            expect(page).to have_content('Rejected Friendship')
+            expect(page).to have_content('You reject an Invitation')
         end         
     end
 end
 
-
-
-# describe Calculator do
-#     describe "#add" do
-#         it "returns the sum of two numbers" do
-#             #calculator = Calculator.new
-#             expect(5+2).to eql(7)
-#         end
-#     end
-# end
