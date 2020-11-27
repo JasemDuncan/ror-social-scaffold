7d62879cbb821a6f1a25c65e683ea384de523c68
JasemDunan <jasemvalencia@gmail.com>
2020-11-27 15:53:35 -0500

    We created before de method Confirm_friend wich create 2 rows for friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,9 +41,9 @@ class User < ApplicationRecord
     friendship = inverse_friendships.find { |friend| friend.user == user }
     friendship2 = friendships.build
     friendship2.user_id = id
-    friendship2.friend_id = user.id
-    friendship2.confirmed = true
+    friendship2.friend_id = user.id    
     friendship.confirmed = true
+    friendship2.confirmed = true
     friendship.save
     friendship2.save
   end
